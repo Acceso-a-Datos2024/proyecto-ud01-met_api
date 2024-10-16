becae5b863b6193571073a859a8eb78459b07bde
Manue14 <mnalvarezgomez@gmail.com>
2024-10-16 22:34:15 +0200

    Funcionalidad de mostrar una imagen aleatoria con su título

--- proyectoUD1_metAPI/src/main/java/com/example/proyectoud1_metapi/ByeController.java
+++ proyectoUD1_metAPI/src/main/java/com/example/proyectoud1_metapi/ByeController.java
@@ -1,16 +1,32 @@
 package com.example.proyectoud1_metapi;
 
+import com.example.proyectoud1_metapi.Model.ApiRequester;
+import com.example.proyectoud1_metapi.Model.ArtPiece;
 import javafx.fxml.FXML;
 import java.io.IOException;
-
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class ByeController{
-
+    @FXML
+    private ImageView imageView;
+    
+    @FXML
+    private Label welcomeText;
 
     @FXML
     protected void byeCambiar() throws IOException {
         HelloApplication.setRoot("hello-view");
+    }
+    
+    @FXML
+    protected void randomImg() {
+        ApiRequester requester = new ApiRequester();
+        ArtPiece artPiece = requester.getRandomArtPiece();
+        Image image = new Image(artPiece.getPrimaryImage());
+        imageView.setImage(image);
+        welcomeText.setText(artPiece.getTitle());
     }
 
 
